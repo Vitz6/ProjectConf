e6e130ab324be69400ae972b35e9225e5cbb4554
wincenty zalewski <wincenty zalewski>
2016-04-28 16:19:08 +0200

    Przyklad

---
+++ IdeaProjects/ProjectConf/src/main/webapp/WEB-INF/static/scripts/index.js
@@ -0,0 +1,29 @@
+var app = angular.module('myApp', []);
+app.controller('showController', function ($scope, $http) {
+    
+   //var getEmployeesUrl = 'employees';
+    var getCommentUrl = 'getcomments';
+    
+    $scope.setCommnts = function () {
+
+        $http.post('getCommentUrl', "sad").then(successCallback, errorCallback);
+
+    }
+
+    $scope.getComments = function () {
+       $http.get('getcomments').success(function (data) {
+            $scope.comments = data;
+        }).error(function (data, status) {
+            alert("Error");
+        });
+    }
+
+
+
+    $scope.SendComment= function (comment) {
+        //alert(comment);
+        return $http.post('setcomment', comment);
+    }
+
+
+});
